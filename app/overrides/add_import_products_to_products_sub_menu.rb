cd9d0be868187a6064ed32cd290eb81f97e5539c
Daniel <znowm4n@gmail.com>
2018-02-22 10:22:57 +0000

    Updated

--- app/overrides/add_import_products_to_products_sub_menu.rb
+++ app/overrides/add_import_products_to_products_sub_menu.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(
   :virtual_path => 'spree/admin/shared/sub_menu/_product',
   :name => 'import_products_tab',
+  :original => '5c6807c2920b4280184cbdf867b7ba98e41b576f',
   :insert_bottom => "#sidebar-product",
   :text => %Q{ <%= tab :product_imports %> }
 )
