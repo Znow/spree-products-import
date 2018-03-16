22539b32bbd460ff139442141175981a4a4ceab6
Daniel <znowm4n@gmail.com>
2018-03-16 10:26:48 +0000

    Updated product import stuff

--- app/models/spree/product_import.rb
+++ app/models/spree/product_import.rb
@@ -3,10 +3,12 @@ require 'csv'
 class Spree::ProductImport < ActiveRecord::Base
 
   # CONSTANTS
-  IMPORTABLE_PRODUCT_FIELDS = [:slug, :name, :price, :cost_price, :available_on, :shipping_category,
-                               :tax_category, :taxons, :option_types, :description].to_set
-  IMPORTABLE_VARIANT_FIELDS = [:sku, :slug, :cost_price, :cost_currency, :tax_category,
-                               :stock_items_count, :option_values].to_set
+  # IMPORTABLE_PRODUCT_FIELDS = [:slug, :name, :price, :cost_price, :available_on, :shipping_category, :tax_category, :taxons, :option_types, :description].to_set
+  IMPORTABLE_PRODUCT_FIELDS = ["EAN", "ItemUnit", "Nettopris", "Bruttopris", "LangProduktBeskrivelse", "ProduktGruppe", "ProduktID", 
+                               "Varetekst1", "Varetekst2", "Synonyms", "ProduktGruppeTekst", "Weight", "SupName", "SupplierURL", 
+                               "ProductURL", "PakkeAntal", "Billede", "Kategori1", "Kategori2", "Kategori3", "Kategori4", 
+                               "Specifications", "SupplierProductNumber", "DisplayName", "Brand"].to_set
+  IMPORTABLE_VARIANT_FIELDS = [:sku, :slug, :cost_price, :cost_currency, :tax_category, :stock_items_count, :option_values].to_set
 
   # Not directly assignable to the product
   RELATED_PRODUCT_FIELDS = [:taxons, :option_types].to_set
@@ -36,21 +38,24 @@ class Spree::ProductImport < ActiveRecord::Base
     import_variant_data if variants_csv.present?
   end
 
-  #handle_asynchronously :start_product_import ??????????????
+  #handle_asynchronously :start_product_import# ??????????????
 
   def import_product_data
+    #byebug
     failed_import = []
-    CSV.foreach(products_csv.path, headers: true, header_converters: :symbol, encoding: Encoding::UTF_8) do |product_data|
+    Spree::Product.destroy_all if Spree::Product.all.any?
+    #CSV.foreach(products_csv.path, headers: true, header_converters: :symbol, encoding: Encoding::UTF_8, col_sep: ';') do |product_data|
+    CSV.foreach(products_csv.path, encoding: 'iso-8859-1', headers: true, col_sep: ';') do |product_data|
       unless import_product_from(product_data)
         failed_import << product_data
       end
     end
-    if failed_import.empty?
-      Spree::ProductImportMailer.import_data_success_email(id, "products_csv").deliver_later
-    else
-      failed_import_csv = build_csv_from_failed_import_list(failed_import)
-      Spree::ProductImportMailer.import_data_failure_email(id, "products_csv", failed_import_csv).deliver_later
-    end
+    #if failed_import.empty?
+    #  Spree::ProductImportMailer.import_data_success_email(id, "products_csv").deliver_later
+    #else
+    #  failed_import_csv = build_csv_from_failed_import_list(failed_import)
+    #  Spree::ProductImportMailer.import_data_failure_email(id, "products_csv", failed_import_csv).deliver_later
+    #end
   end
 
   def import_variant_data
@@ -67,11 +72,27 @@ class Spree::ProductImport < ActiveRecord::Base
       Spree::ProductImportMailer.import_data_failure_email(id, "variants_csv", failed_import_csv).deliver_later
     end
   end
+  
+  def import_product_from(product_data_row)
+    begin
+      ActiveRecord::Base.transaction do
+        product = create_or_update_product(product_data_row)
+        set_missing_product_options(product, product_data_row)
+        add_taxons(product, product_data_row)
+        add_images(product, product_data_row[:images])
+      end
+    rescue Exception
+      false
+    else
+      true
+    end
+  end
 
   def create_or_update_product(product_data_row)
+    #byebug
     product_properties = build_properties_hash(product_data_row, IMPORTABLE_PRODUCT_FIELDS, RELATED_PRODUCT_FIELDS)
-    product_properties[:tax_category] = Spree::TaxCategory.find_or_create_by!(name: product_properties[:tax_category])
-    product_properties[:shipping_category] = Spree::ShippingCategory.find_or_create_by!(name: product_properties[:shipping_category])
+    product_properties[:tax_category] = Spree::TaxCategory.first#find_or_create_by!(name: product_properties[:tax_category])
+    product_properties[:shipping_category] = Spree::ShippingCategory.first#.find_or_create_by!(name: product_properties[:shipping_category])
     product = Spree::Product.find_or_initialize_by(slug: product_properties[:slug])
     product.update!(product_properties)
     product
@@ -92,21 +113,6 @@ class Spree::ProductImport < ActiveRecord::Base
   def add_taxons(product, product_data_row)
     product_data_row[:taxons].to_s.split(',').each do |taxon|
       product.taxons.find_or_create_by!(name: taxon)
-    end
-  end
-
-  def import_product_from(product_data_row)
-    begin
-      ActiveRecord::Base.transaction do
-        product = create_or_update_product(product_data_row)
-        set_missing_product_options(product, product_data_row)
-        add_taxons(product, product_data_row)
-        add_images(product, product_data_row[:images])
-      end
-    rescue Exception
-      false
-    else
-      true
     end
   end
 
@@ -131,6 +137,39 @@ class Spree::ProductImport < ActiveRecord::Base
         variant.option_values << option_value
       end
     end
+  end
+  
+  def build_properties_hash(data_row, attributes_to_read, related_attr)
+    #byebug
+    properties_hash = {}
+    copieable_attributes = (attributes_to_read - related_attr)
+
+    data_row.each do |key, value|
+      if copieable_attributes.include? key
+        case key
+          when "DisplayName"
+            properties_hash["name"] = value
+            properties_hash["meta_title"] = value
+            properties_hash["slug"] = value.parameterize
+          when "Nettopris"
+            properties_hash["cost_price"] = value.gsub(',','.')#.to_f / 100
+          when "Bruttopris"
+            properties_hash["price"] = value.gsub(',','.')#.to_f / 100
+          when "LangProduktBeskrivelse"
+            properties_hash["description"] = value
+            properties_hash["meta_description"] = value
+          when "SupplierProductNumber"
+            properties_hash["sku"] = value
+        end
+        properties_hash["available_on"] = Time.now.utc
+        #properties_hash["shipping_category"] = 1
+        properties_hash["promotionable"] = true
+        #properties_hash["tax_category"] = 
+        #properties_hash["taxons"] = "tax1, tax2, tax3"
+        
+      end
+    end
+    properties_hash
   end
 
   def import_variant_from(variant_data_row)
@@ -157,17 +196,7 @@ class Spree::ProductImport < ActiveRecord::Base
     end
   end
 
-  def build_properties_hash(data_row, attributes_to_read, related_attr)
-    properties_hash = {}
-    copieable_attributes = (attributes_to_read - related_attr)
-
-    data_row.each do |key, value|
-      if copieable_attributes.include? key
-        properties_hash[key] = value
-      end
-    end
-    properties_hash
-  end
+  
 
   def add_images(model_obj, image_dir)
     return unless image_dir
